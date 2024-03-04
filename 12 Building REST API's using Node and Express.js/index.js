936a9937f7270095fd555d40a4a67124057bbef7
Himanshu <himanshuparodiya@gmail.com>
2024-03-04 12:01:29 +0530

    Make a hybrid server to render direct html and JSON format for react etc,
    render all users
    render each user by id (With Dynamic path parameter)

--- 12 Building REST API's using Node and Express.js/index.js
+++ 12 Building REST API's using Node and Express.js/index.js
@@ -1,11 +1,36 @@
 const express = require('express');
-
+const users = require("./MOCK_DATA.json")
 const app = express();
 const PORT = 8000;
 
 
 app.get("/", (req, res) => {
-    res.send("Hello this is home page")
+    return res.send("Go to /users")
+})
+app.get("/users", (req, res) => {
+    const html = `
+    <ul>
+        ${users.map((user) => (
+        `<li>${user.first_name}</li>`
+    )).join("")}
+    </ul>
+    `
+    return res.send(html)
+})
+
+// REST API 
+// get all users
+app.get("/api/users", (req, res) => {
+    return res.json(users)
+})
+
+// get each users
+app.get("/api/users/:id", (req, res) => {
+    const id = +req.params.id;
+    const userWithId = users.find((user) => (
+        user.id == id
+    ))
+    return res.json(userWithId)
 })
 
 app.listen(PORT, () => {
