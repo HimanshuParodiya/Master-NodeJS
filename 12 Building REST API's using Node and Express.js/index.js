2609f6eeb2d532d54cbf82f3cb1b8e4f8ce51bb5
Himanshu <himanshuparodiya@gmail.com>
2024-03-04 12:12:42 +0530

    initialize patch post and delete method

--- 12 Building REST API's using Node and Express.js/index.js
+++ 12 Building REST API's using Node and Express.js/index.js
@@ -33,6 +33,65 @@ app.get("/api/users/:id", (req, res) => {
     return res.json(userWithId)
 })
 
+
+// by default browser will do get request
+
+// Post requests
+
+app.post('/api/users', (req, res) => {
+    // TODO: create new user 
+
+    return res.json({ status: "Pending" })
+})
+
+// Patch requests
+
+app.patch('/api/users/:id', (req, res) => {
+    // TODO: edit the user with id 
+
+    return res.json({ status: "Pending" })
+})
+
+// Delete requests
+
+app.delete('/api/users/:id', (req, res) => {
+    // TODO: Delete the user with id 
+
+    return res.json({ status: "Pending" })
+})
+
+// now we can see the path of get, put and delete is same 
+//- so we can make their group
+
+// like 
+
+app.route("/api/users/:id").get((req, res) => {
+    const id = +req.params.id;
+    const userWithId = users.find((user) => (
+        user.id == id
+    ))
+    return res.json(userWithId)
+}).patch((req, res) => {
+    // TODO: edit the user with id 
+
+    return res.json({ status: "Pending" })
+}).delete((req, res) => {
+    // TODO: edit the user with id 
+
+    return res.json({ status: "Pending" })
+})
+
+
+
+
 app.listen(PORT, () => {
     console.log(`Server started at PORT: http://localhost:${PORT}`);
 })
+
+
+
+
+
+// now we have worked with get method
+
+// in next we will work with POSTMAN to work with PATCH POST AND DELETE Method
