5be2fcc7b60dc75415eb2bc86129d690cb3c3cde
Himanshu <himanshuparodiya@gmail.com>
2024-03-08 22:29:36 +0530

    Redirecting to URL

--- 20 URL Shortener/index.js
+++ 20 URL Shortener/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const urlRoute = require('./routes/url');
 const { connectToMongoDB } = require('./connect');
+const URL = require('./models/url');
+
 const app = express();
 const PORT = 8001;
 
@@ -12,6 +14,21 @@ connectToMongoDB("mongodb://127.0.0.1:27017/url-shortener")
 
 // middleware 
 app.use(express.json())
+
+app.get("/:shortID", async (req, res) => {
+    const shortID = req.params.shortID
+    const entry = await URL.findOneAndUpdate({
+        shortID
+    }, {
+        $push: {
+            visitHistory: {
+                timestamp: Date.now()
+            },
+        }
+    })
+
+    res.redirect(entry.redirectedURL)
+})
 
 app.use("/url", urlRoute)
 app.listen(PORT, () => {
